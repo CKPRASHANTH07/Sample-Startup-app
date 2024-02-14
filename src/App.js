3a3f7acb8513b7715b6b72b006eab5e65ffb56c1
CKPRASHANTH07 <ckprashanthc@gmail.com>
2024-02-14 22:40:13 +0530

    modified:   src/App.js 	modified:   src/Pagecompos/Training.js 	new file:   src/Pagecompos/pageimg/dogo.png 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from "./Pagecompos/Home";
 import Aboutus from "./Pagecompos/Aboutus";
 import Footer from "./UIcomp/Footer";
 import Services from "./Pagecompos/Services";
+import Training from "./Pagecompos/Training";
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
       <Services />
+      <Training />
       <Aboutus />
       <Footer />
     </div>
