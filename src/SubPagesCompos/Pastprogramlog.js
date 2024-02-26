c4914a03bc61b3d9c775abb3284e472d54d8459d
keshavkumar@03 <keshavkumark03@gmail.com>
2024-02-26 23:11:07 +0530

    events completed

--- src/SubPagesCompos/Pastprogramlog.js
+++ src/SubPagesCompos/Pastprogramlog.js
@@ -2,7 +2,155 @@ import React from 'react'
 
 function Pastprogramlog() {
   return (
-    <div>Pastprogramlog</div>
+
+    <section  className="grid gap-4    py-32 bgcolor place-items-center">
+<h1 className="grid my-4 text-6xl font-bold text-blue-900 place-items-center ">past events</h1>
+    <div className="relative items-center  px-5 py-12 mx-auto md:px-12 lg:px-24 max-w-7xl">
+        <div className="grid w-full grid-cols-1 gap-8 mx-auto lg:grid-cols-6">
+            <div className="p-6 w-90 col-span-2 transition-all duration-300 border-2 border-solid  border-gray-500 transform  shadow-lg place-items-center rounded-xl hover:-translate-y-2 hover:shadow-2xl">
+                <h1 className="mx-auto mb-8 text-2xl  text-blue-900  text-center font-semibold leading-none tracking-tighter lg:text-3xl">Master OOPS in 2hrs </h1>
+                <img className="object-contain object-center w-1/2 m-auto lg:h-48 md:h-36 rounded-xl" src= "images/app_dev.png" alt="blog"/>
+
+                <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                   <b>Date:</b> 12th jan 2024 3:00pm   </p>       
+                   <br></br>         
+                    <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing quis laboris nisi   </p>                
+                  <div className="mt-16">
+      <a href="/appdevelopment" className="relative inline-block text-sm group">
+       <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-blue-900 transition-colors duration-300 ease-out border-2 border-blue-900 rounded-lg group-hover:text-white">
+        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+       <span className="absolute left-0 w-40 h-40 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-blue-900 group-hover:-rotate-180 ease"></span>
+      <span className="relative">Learn more</span>
+      </span>
+      <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-blue-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+           </a>
+            
+                     </div>
+               </div>
+            <div className="p-6  col-span-2  transition-all duration-300 transform border-2 border-solid border-gray-500 shadow-lg place-items-center rounded-xl hover:-translate-y-2 hover:shadow-2xl">
+
+            <h1 className="mx-auto mb-8 text-2xl font-semibold text-center leading-none tracking-tighter text-blue-900 lg:text-3xl">master Node.js  </h1>
+                <img className="object-contain object-center  m-auto lg:h-48 md:h-36 rounded-xl" src= "images/web_dev.png" alt="blog"/>
+
+               
+                <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                   <b>Date:</b> 12th jan 2024 3:00pm   </p>       
+                   <br></br>         
+                    <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing quis laboris nisi   </p>
+                  <div className="mt-16">
+      <a href="/webdevelopment" className="relative inline-block text-sm group">
+       <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-blue-900 transition-colors duration-300 ease-out border-2 border-blue-900 rounded-lg group-hover:text-white">
+        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+       <span className="absolute left-0 w-40 h-40 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-blue-900 group-hover:-rotate-180 ease"></span>
+      <span className="relative">Learn more</span>
+      </span>
+      <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-blue-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+           </a>
+            
+                     </div>
+            </div>
+            <div className="p-6   col-span-2 transition-all duration-300 transform border-2 border-solid border-gray-500 shadow-lg place-items-center rounded-xl hover:-translate-y-2 hover:shadow-2xl">
+            <h1 className="mx-auto mb-8 text-2xl font-semibold leading-none text-center tracking-tighter text-blue-900 lg:text-3xl">how to start Digital blogging</h1>
+                <img className="object-contain object-center w-1/2 m-auto lg:h-48 md:h-36 rounded-xl" src= "images/digi_market.png" alt="blog"/>
+
+              
+                <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                   <b>Date:</b> 12th jan 2024 3:40pm   </p>       
+                   <br></br>         
+                    <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing quis laboris nisi   </p>        
+                  <div className="mt-16">    
+      <a href="/digitalmarketing" className="relative inline-block text-sm group">
+       <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-blue-900 transition-colors duration-300 ease-out border-2 border-blue-900 rounded-lg group-hover:text-white">
+        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+       <span className="absolute left-0 w-40 h-40 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-blue-900 group-hover:-rotate-180 ease"></span>
+      <span className="relative">Learn more</span>
+      </span>
+      <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-blue-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+           </a>
+            
+                     </div>
+            </div>          
+        </div>
+    </div>
+
+
+    <div className="relative items-center  px-5 py-12 mx-auto md:px-12 lg:px-24 max-w-7xl">
+        <div className="grid w-full grid-cols-1 gap-8 mx-auto lg:grid-cols-6">
+            <div className="p-6 w-90 col-span-2 transition-all duration-300 border-2 border-solid  border-gray-500 transform  shadow-lg place-items-center rounded-xl hover:-translate-y-2 hover:shadow-2xl">
+                <h1 className="mx-auto mb-8 text-2xl  text-blue-900  text-center font-semibold leading-none tracking-tighter lg:text-3xl"> how to get started with design thinking</h1>
+                <img className="object-contain object-center w-1/2 m-auto lg:h-48 md:h-36 rounded-xl" src= "images/app_dev.png" alt="blog"/>
+
+                <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                   <b>Date:</b> 10th jan 2024 6:00am   </p>       
+                   <br></br>         
+                    <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing quis laboris nisi   </p>            
+                  <div className="mt-16">
+      <a href="/appdevelopment" className="relative inline-block text-sm group">
+       <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-blue-900 transition-colors duration-300 ease-out border-2 border-blue-900 rounded-lg group-hover:text-white">
+        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+       <span className="absolute left-0 w-40 h-40 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-blue-900 group-hover:-rotate-180 ease"></span>
+      <span className="relative">Learn more</span>
+      </span>
+      <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-blue-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+           </a>
+            
+                     </div>
+               </div>
+            <div className="p-6  col-span-2  transition-all duration-300 transform border-2 border-solid border-gray-500 shadow-lg place-items-center rounded-xl hover:-translate-y-2 hover:shadow-2xl">
+
+            <h1 className="mx-auto mb-8 text-2xl font-semibold text-center leading-none tracking-tighter text-blue-900 lg:text-3xl">Unity Development using CPP</h1>
+                <img className="object-contain object-center  m-auto lg:h-48 md:h-36 rounded-xl" src= "images/web_dev.png" alt="blog"/>
+
+                <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                   <b>Date:</b> 16th jan 2024 12:00pm   </p>       
+                   <br></br>         
+                    <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing quis laboris nisi   </p>
+              
+                  <div className="mt-16">
+      <a href="/webdevelopment" className="relative inline-block text-sm group">
+       <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-blue-900 transition-colors duration-300 ease-out border-2 border-blue-900 rounded-lg group-hover:text-white">
+        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+       <span className="absolute left-0 w-40 h-40 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-blue-900 group-hover:-rotate-180 ease"></span>
+      <span className="relative">Learn more</span>
+    
+      </span>
+      <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-blue-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+           </a>
+            
+                     </div>
+            </div>
+            <div className="p-6   col-span-2 transition-all duration-300 transform border-2 border-solid border-gray-500 shadow-lg place-items-center rounded-xl hover:-translate-y-2 hover:shadow-2xl">
+            <h1 className="mx-auto mb-8 text-2xl font-semibold leading-none text-center tracking-tighter text-blue-900 lg:text-3xl">aws workshop</h1>
+                <img className="object-contain object-center w-1/2 m-auto lg:h-48 md:h-36 rounded-xl" src= "images/digi_market.png" alt="blog"/>
+
+              
+                <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                   <b>Date:</b> 13th jan 2024 8:00pm   </p>       
+                   <br></br>         
+                    <p className="mx-auto text-base leading-relaxed  font-semibold text-gray-800">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing quis laboris nisi   </p>       
+                  <div className="mt-16">    
+      <a href="/digitalmarketing" className="relative inline-block text-sm group">
+       <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-blue-900 transition-colors duration-300 ease-out border-2 border-blue-900 rounded-lg group-hover:text-white">
+        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+       <span className="absolute left-0 w-40 h-40 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-blue-900 group-hover:-rotate-180 ease"></span>
+      <span className="relative">Learn more</span>
+      </span>
+      <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-blue-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+           </a>
+            
+                     </div>
+            </div>          
+        </div>
+    </div>
+
+</section>
+
   )
 }
 
