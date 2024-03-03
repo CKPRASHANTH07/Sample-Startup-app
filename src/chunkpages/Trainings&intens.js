467c0d83badf9e48833161d75345e8911f8ad812
CKPRASHANTH07 <ckprashanthc@gmail.com>
2024-03-03 12:05:26 +0530

    modified:   src/UIcomp/Footer.js 	modified:   src/chunkpages/Trainings&intens.js

--- src/chunkpages/Trainings&intens.js
+++ src/chunkpages/Trainings&intens.js
@@ -5,7 +5,7 @@ const Trainingsintens = () => {
   return (
     <div className='w-96 md:w-full space-y-16 '>
         <h1 className='text-4xl pl-4 font-bold'>Training&Internships</h1>
-<div className=" items-start pl-10  flex flex-rows lg:flex-col overflow-x-auto py-20 md:py-0">
+<div className=" items-start md:w-[670px] sm:[470px] lg:w-[1000px] xl:w-full  flex flex-rows lg:flex-col overflow-x-auto md:py-0">
         <div className="flex ">
       <div className="border-2 p-8 border-gray-300 rounded-2xl h-96 w-96">
         <h1 className="text-3xl pb-4 font-bold">Internships</h1>
